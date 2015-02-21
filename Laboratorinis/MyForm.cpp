26b14838f93b76b3d702135a175b95e1cde95526
AmberTheTurtle <gintrexe@gmail.com>
2015-02-21 19:24:15 +0200

    Tuscias projektas

---
+++ Laboratorinis/MyForm.cpp
@@ -0,0 +1,14 @@
+﻿#include "MyForm.h"
+
+//------------------------------------------------------------
+using namespace System;
+using namespace System::Windows::Forms;
+[STAThread]
+int main(array<System::String ^> ^args)
+{
+ Application::EnableVisualStyles();
+ Application::SetCompatibleTextRenderingDefault(false);
+ Laboratorinis::MyForm form; // Eilutė, kuri gali būti koreguojama
+ Application::Run(%form);
+}
+//------------------------------------------------------------
